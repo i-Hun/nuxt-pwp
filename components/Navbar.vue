59c24f998cba115b82787dda9b0d255d470344d3
Oleg Nagornyy <hun@Olegs-MacBook-Pro.local>
2019-01-06 15:12:13 +0300

    sidenote, toc

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,6 +1,8 @@
 <template>
 	<nav class="navbar" role="navigation" aria-label="main navigation">
-		<nuxt-link class="navbar-item" to="/" active-class="is-active">Oleg Nagornyy</nuxt-link>
+
+		<nuxt-link class="navbar-item logo" to="/" active-class="is-active" v-if="$nuxt.$route.path !== '/'">Oleg Nagornyy</nuxt-link>
+		<span class="navbar-item logo" v-if="$nuxt.$route.path === '/'">Oleg Nagornyy</span>
 		<nuxt-link class="navbar-item" to="/data-science/" active-class="is-active">Data Science</nuxt-link>
 		<nuxt-link class="navbar-item" to="/intro2python/" active-class="is-active">Introduction to Python</nuxt-link>
 		<nuxt-link class="navbar-item" to="/events/" active-class="is-active">Events</nuxt-link>
@@ -12,6 +14,9 @@
 		margin-right: 20px;
 	}
 	.is-active {
+		font-weight: 700;
+	}
+	.logo {
 		font-weight: 700;
 	}
 </style>
--- pages/data-science/_id.vue
+++ pages/data-science/_id.vue
@@ -1,19 +1,30 @@
 <template>
 	<section class="section post content">
+		<div class="js-toc"></div>
 		<div v-for="cell in cells" class="cell">
 			<!-- исправить вложенность -->
 			<div v-if="cell.cell_type === 'code'" class="code-cell">
 				<pre class="line-numbers"><code class="language-python" v-html="cell.source.join('')"></code></pre>
 			</div>
-			<div v-if="cell.hasOwnProperty('outputs') && cell.outputs.length">
-				<div v-for="output in cell.outputs">
+			<div v-if="cell.hasOwnProperty('outputs') && cell.outputs.length" class="code-outputs">
+				<div v-for="output in cell.outputs" class="output">
 					<div v-if="output.hasOwnProperty('data') && output.data.hasOwnProperty('image/svg+xml')"
-						v-html="output.data['image/svg+xml'].join('')"></div>
+						v-html="output.data['image/svg+xml'].join('')" class="output-svg"></div>
 					<div v-if="output.hasOwnProperty('data') && output.data.hasOwnProperty('text/html')"
-						v-html="output.data['text/html'].join('')"></div>
+						v-html="output.data['text/html'].join('')" class="output-text-html"></div>
+<!-- 					<div v-if="output.hasOwnProperty('data') && output.data.hasOwnProperty('text/plain')" class="output-text-plain">
+						{{output.data['text/plain'].join('')}}
+					</div> -->
 				</div>
 			</div>
-			<div class='text-cell' v-if="cell.cell_type === 'markdown'" v-html="markdowned(cell.source.join(''))">
+			<div class='text-cell' v-if="(cell.cell_type === 'markdown') & !('references' in cell.metadata)" v-html="markdowned(cell.source.join(''))">
+			</div>
+
+			<div class='text-cell references' v-if="(cell.cell_type === 'markdown') & ('references' in cell.metadata)">
+				<details>
+					<summary><strong>References</strong></summary>
+					<div v-html="markdowned(cell.source.join(''))"></div>
+				</details>		
 			</div>
 		</div>
 		<div class="comments" v-if="isShowComments">
@@ -38,7 +49,8 @@ var md = require('markdown-it')({
 	html: true,
 	linkify: true,
 	typographer: true
-}).use(require('markdown-it-mathjax')());
+}).use(require('markdown-it-named-headings'))
+//.use(require('markdown-it-mathjax')());
 
 export default {
 	name: 'data-science-page',
@@ -48,10 +60,12 @@ export default {
 	head: {
 		link: [
 			{ rel: 'stylesheet', href: '/css/prism.css' },
+			// { rel: 'stylesheet', href: 'https://cdnjs.cloudflare.com/ajax/libs/tocbot/4.4.2/tocbot.css' },
 		],
 		script: [
 			{ src: 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.5/MathJax.js?config=TeX-MML-AM_CHTML', ssr: false },
 			{ src: '/js/prism.js', ssr: false },
+			{ src: 'https://cdnjs.cloudflare.com/ajax/libs/tocbot/4.4.2/tocbot.js', ssr: false },
 			// { src: 'https://cdnjs.cloudflare.com/ajax/libs/prism/1.15.0/components/prism-python.js', ssr: false },
 			// { src: 'https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.13.1/highlight.min.js', ssr: false }
 		]
@@ -62,14 +76,35 @@ export default {
 		}
 	},
 	mounted(){
+
+		MathJax.Hub.Config({
+			extensions: ["tex2jax.js"],
+			jax: ["input/TeX", "output/HTML-CSS"],
+			tex2jax: {
+				inlineMath: [['$','$']],
+				displayMath: [['$$','$$']],
+				processEscapes: true
+			},
+			"HTML-CSS": { fonts: ["TeX"] }
+		});
+		
 		MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
 
-		// hljs.initHighlightingOnLoad();
+
+		tocbot.init({
+			// Where to render the table of contents.
+			tocSelector: '.js-toc',
+			// Where to grab the headings to build the table of contents.
+			contentSelector: '.content',
+			// Which headings to grab inside of the contentSelector element.
+			headingSelector: 'h2, h3',
+		});
 
 		Prism.highlightAll();
 	},
 	asyncData (context) {
 		let ipynb = require(`~/assets/notebooks/data-science/${context.params.id}.ipynb`);
+
 		return {
 			cells: JSON.parse(ipynb)["cells"]
 		}
@@ -77,20 +112,75 @@ export default {
 	methods: {
 		markdowned: function (text) {
 			var result = md.render(text);
-			result = result.replace("img/", "/img/content/")
-			return result
+			result = result.replace("img/", "/img/content/");
+			return result;
 		}
 	}
 }
 </script>
 
-<style>
+<style lang="scss">
 	img {
 		margin: 10px;
 	}
 
 	.comments {
 		margin-top: 5rem;
-		margin-bottom: 5rem;
+		margin-bottom: 2rem;
 	}
+
+	.note {
+		font-size: .9em;
+			float: right;
+			margin: -5rem -13rem 0 0rem;
+			width: 25%;
+			line-height: normal;
+			padding: 10px;
+			background: #eee;
+	}
+
+	.js-toc {
+		position: fixed;
+			top: 10px;
+			right: 10px;
+			width: 20%;
+			font-size: 0.8rem;
+			color: #bbb;
+
+
+			a {
+				opacity: 0.2;
+				text-decoration: none;
+				color: black;
+
+				&:hover {
+				text-decoration: underline;
+				}
+			}
+
+			&:hover {
+				opacity: 1;
+				a {
+					opacity: 1;
+				}
+			}
+	}
+
+	@media screen and (max-width: 1280px) {
+		.js-toc {
+			display: none;
+		}
+	}
+
+
+	.is-active-li {
+		font-weight: 900;
+		color: #000;
+
+			a {
+				opacity: 1;
+			}
+	}
+
+
 </style>
