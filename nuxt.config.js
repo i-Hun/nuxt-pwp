2b4b4391307fabdf32fc12bc93a459b02ab86043
Oleg Nagornyy <nagornyy.o@gmail.com>
2019-06-04 15:59:21 +0300

    В netlify отсутствует функция flat. Добавил её через прототипы

--- nuxt.config.js
+++ nuxt.config.js
@@ -27,6 +27,13 @@ const eventsUrls = events.map((event) => {
 //	console.error(err);
 // });
 
+Object.defineProperty(Array.prototype, 'flat', {
+    value: function(depth = 1) {
+      return this.reduce(function (flat, toFlatten) {
+        return flat.concat((Array.isArray(toFlatten) && (depth>1)) ? toFlatten.flat(depth-1) : toFlatten);
+      }, []);
+    }
+});
 
 let allRoutes = coursesUrls.flat().concat(eventsUrls);
 
